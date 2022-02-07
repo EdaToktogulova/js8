495dc4973f9ece9069132a5ed10f690b19f92d78
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-02-07 14:20:12 +0600

    news

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-// Method 1.
+// // Method 1.
 
-//Create
-document.body.innerHTML = "<h1>Hello world</h1>";
-//Find
-let h1Element = document.querySelector('h1');
-//Modify
-h1Element.style.color = "rgb(255, 0, 0)";
+// //Create
+// document.body.innerHTML = "<h1>Hello world</h1>";
+// //Find
+// let h1Element = document.querySelector('h1');
+// //Modify
+// h1Element.style.color = "rgb(255, 0, 0)";
 
-// Method 2.
+// // Method 2.
 
-// Create
-let h2Elemet = document.createElement('h2');
-//Attach
-document.body.prepend(h2Elemet);
-//Modify
-h2Elemet.textContent = "Nice to meet you!";
+// // Create
+// let h2Elemet = document.createElement('h2');
+// //Attach
+// document.body.prepend(h2Elemet);
+// //Modify
+// h2Elemet.textContent = "Nice to meet you!";
+
+let exercisesElement = document.querySelector('#exercises');
+
+let h1Element = document.createElement ('h1'); // create
+h1Element.textContent = "H1 header"; // modify
+exercisesElement.append(h1Element); // attach
+
+let h2Elemet = document.createElement ('h2'); 
+h2Elemet.textContent = "H2 header";
+exercisesElement.append(h2Elemet);
