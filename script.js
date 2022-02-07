08042891b9af37ec80e359f7e7667a18aab12d0c
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-02-07 20:01:34 +0600

    added news

--- script.js
+++ script.js
@@ -1,60 +1,62 @@
-// // Method 1.
+// Method 1.
 
-// //Create
+// // Create
 // document.body.innerHTML = "<h1>Hello world</h1>";
-// //Find
+// // Find
 // let h1Element = document.querySelector('h1');
-// //Modify
+// // Modify
 // h1Element.style.color = "rgb(255, 0, 0)";
 
 // // Method 2.
 
 // // Create
-// let h2Elemet = document.createElement('h2');
-// //Attach
-// document.body.prepend(h2Elemet);
-// //Modify
-// h2Elemet.textContent = "Nice to meet you!";
+// let h2Element = document.createElement('h2');
+// // Attach
+// document.body.prepend(h2Element);
+// // Modify
+// h2Element.textContent = "Nice to meet you!";
 
 let exercisesElement = document.querySelector('#exercises');
-// append - incert element before closing 
+// append - insert element before closing tag
+// prepend - insert element after opening tag
+// before - insert element before opening tag
+// after - insert element after closing tag
 
-let h1Element = document.createElement ('h1'); // create
+let h1Element = document.createElement('h1'); // create
 h1Element.textContent = "H1 header"; // modify
 exercisesElement.append(h1Element); // attach
 
-let h2Element = document.createElement ('h2'); 
+let h2Element = document.createElement('h2');
 h2Element.textContent = "H2 header";
-exercisesElement.append(h2Element); // attach
+exercisesElement.append(h2Element);
 
-let h3Element = document.createElement ('h3');
-h3Element.textContent = "H3 header";
-exercisesElement.append(h3Element);
+let h3Element = document.createElement('h3'); // create
+exercisesElement.append(h3Element); // attach
+h3Element.textContent = "H3 header"; // modify
 
-let h4Element = document.createElement ('h4'); 
-h4Element.textContent = "H4 header"; 
-exercisesElement.prepend(h4Element);
+let h4Element = document.createElement('h4'); // create
+exercisesElement.prepend(h4Element); // attach
+h4Element.textContent = "H4 header"; // modify
 
+let h5Element = document.createElement('h5'); // create
+exercisesElement.before(h5Element); // attach
+h5Element.textContent = "H5 header"; // modify
 
-let h5Element = document.createElement ('h5'); 
-h5Element.textContent = "H5 header"; 
-exercisesElement.before(h5Element);
+let h6Element = document.createElement('h6'); // create
+exercisesElement.after(h6Element); // attach
+h6Element.textContent = "H6 header"; // modify
 
+// Exercise
+let exercise1 = document.querySelector('#exercise1');
+exercise1.before('before');
+exercise1.prepend('prepend');
+exercise1.append('append');
+exercise1.after('after');
+// exercise1.replaceWith('replaced');
 
-let h6Element = document.createElement ('h6'); 
-h6Element.textContent = "H6 header"; 
-exercisesElement.after(h6Element);
-
-//Exercise
-let Exercise1 = document.querySelector('#execise1');
-Exercise1.before('before');
-Exercise1.prepend('prepend');
-Exercise1.append('append');
-Exercise1.after('after');
-
-let button = document.createElement ('button');
+let button = document.createElement('button');
 exercisesElement.prepend(button);
-button.textContent = "Lauch Meeting";
+button.textContent = "Launch Meeting";
 button.style.padding = "10px 25px";
 button.style.color = "white";
 button.style.borderRadius = "10px";
@@ -63,57 +65,43 @@ button.style.border = "none";
 
 let shoppingList = ['Apple', 'Banana', 'Pinapple'];
 // Create UL
-let listElement = document.createElement ('ul');
-exercisesElement.prepend(ul);
+let listElement = document.createElement('ul');
+exercisesElement.prepend(listElement);
 // Create LI
 for (let i = 0; i < shoppingList.length; i++) {
-   let listElement = document.createElement('li');
-listElement.append(listItemElement);
-listItemElement.textContent = shoppingList[i];
-}
-
-//Create table
-let table = document.createElement('table');
-exercisesElement.prepend(table);
-// Create LI
-for (let i = 0; i < shoppingList.length; i++) {
-   let listElement = document.createElement('li');
-listElement.append(listItemElement);
-listItemElement.textContent = shoppingList[i];
-}
-
-// How to copy 
-let button2 = button.cloneNode ();
-button2.textContent = "Hello";
-exercisesElement.prepend(button2);
-
-let button3 = button.cloneNode ();
-button3.textContent = "Hello";
-exercisesElement.prepend(button3);
-
-let button4 = button.cloneNode ();
-button4.textContent = "Hello";
-exercisesElement.prepend(button4);
-
-let button5 = button.cloneNode ();
-button5.textContent = "Hello";
-exercisesElement.prepend(button5);
-
-let button6 = button.cloneNode ();
-button6.textContent = "Hello";
-exercisesElement.prepend(button6);
-
-// How to remove
-button2.remove();
-
-let listElement = document.createElement('table');
-for (let i = 0; i < shoppingList.length; i++) {
-   let listElement = document.createElement('tr');
-listElement.append(listItemElement);
-listItemElement.textContent = shoppingList[i];
+    let listItemElement = document.createElement('li');
+    listElement.append(listItemElement);
+    listItemElement.textContent = shoppingList[i];
 }
 
 // 0-255 - Math.random() * 255
 
-let clolorful = document.createElement('div');
-clolorful.textContent = "HELLO";
+// Create TABLE
+let table = document.createElement('table');
+exercisesElement.prepend(table);
+// Create LI
+for (let i = 0; i < shoppingList.length; i++) {
+    let tr = document.createElement('tr');
+    table.append(tr);
+    let td = document.createElement('td');
+    tr.append(td);
+    td.style.border = "1px solid black";
+    td.textContent = shoppingList[i];
+}
+
+// How to copy
+let button2 = button.cloneNode();
+button2.textContent = "Hello";
+exercisesElement.prepend(button2);
+
+let list2 = listElement.cloneNode(true);
+exercisesElement.prepend(list2);
+
+// How to remove
+button2.remove();
+
+
+let colorful = document.createElement('div');
+colorful.textContent = "HELLO";
+colorful.style.color = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
+document.body.prepend(colorful);
